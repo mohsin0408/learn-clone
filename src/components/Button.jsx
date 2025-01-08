607e70cd6018c3ce9f06a7ba67e5a4336e82db24
mohsinali shaikh <mohsinalishaikh987@gmail.com>
2025-01-08 16:44:23 +0530

    new

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ text, rounded, px }) => {
 
   return (
     <button
-      className={`border border-[#5A00C7] px-${px} py-3 bg-[#5A00C7] hover:bg-[#42048e] text-white rounded-${rounded} font-semibold`}>
+      className={`border border-[#5A00C7] px-[${px}] py-3 bg-[#5A00C7] hover:bg-[#42048e] text-white rounded-${rounded} font-semibold`}>
       {text}
     </button>
   );
--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -6,29 +6,27 @@ const Curriculum = () => {
   const CurriculumData = {
     heading: "Getting Started",
     data: [
-      "Welcome To The Course!(2:09)",
-      "Course Overview: What You'll Learn &amp; Achieve(1:56)",
-      "Course Focus: AI Programming Tools & Beyond(1:47)",
-      "Understanding AI: Limitations, Costs &amp; Unpredictability(2:19) ",
+      "Welcome To The Course! (2:09)",
+      "Course Overview: What You'll Learn &amp; Achieve  (1:56)",
+      "Course Focus: AI Programming Tools & Beyond  (1:47)",
+      "Understanding AI: Limitations, Costs &amp; Unpredictability  (2:19) ",
     ],
   };
   return (
     <div className="flex flex-col items-center justify-center">
       <h2>Course Curriculum</h2>
       <div>
-        <span className="px-4 py-3">{CurriculumData?.heading}</span>
-        <div className="w-[100%]">
+        <div className="px-4 py-3 bg-[#e8e8e8] ">{CurriculumData?.heading}</div>
+        <div className="w-full text-base">
           {CurriculumData?.data?.map((item, index) => (
             <div
               key={index}
-              className="flex items-center w-[100%] justify-between py-3 px-4 ">
+              className="flex items-center cursor-pointer w-full justify-between py-3 px-4 bg-[#f0f0f0] border-b-2 border-b-white gap-20 hover:text-[#430094] hover:bg-[#e0d2f0] ">
               <span className="flex items-center gap-2 ">
-                <FaYoutube />
+                <FaYoutube className="text-lg " />
                 {item}
               </span>
-              <span>
-                <Button text="Preview" rounded="2xl" px={4} />
-              </span>
+              <Button text="Preview" rounded="2xl" px="12px" />
             </div>
           ))}
         </div>
