e30a17baac73e2d276886bf59f845e967df7d1ac
mohsinali shaikh <mohsinalishaikh987@gmail.com>
2025-01-11 16:52:04 +0530

    new

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -45,28 +45,27 @@ const Course = () => {
       <div className="flex items-center justify-center">
         <div className="grid items-center justify-center grid-cols-1 gap-5 p-4 md:grid-cols-2 ">
           {courseData?.data.map((obj, index) => (
-            <div
+            <Link
+              to="/Curriculum"
               key={index}
               className=" w-[300px] h-full flex flex-col bg-[#f7f7f7] shadow-[0px_1px_#d4baf3] 
             border border-[#d4baf3] rounded-[9px] overflow-hidden cursor-pointer lg:w-[380px] hover:border-[#9d5ee8] hover:border-2 ">
-              <Link to="/Curriculum">
-                <img src={obj.img} className="object-cover " />
-                <div className="flex flex-col justify-between h-full gap-10 px-4 py-5">
-                  <div>
-                    <span className=" text-[#5A00C7] text-lg font-bold ">
-                      {obj?.title}
-                    </span>
-                    <p className="text-sm line-clamp-2">{obj?.desc}</p>
-                  </div>
-                  <div className="flex items-center justify-between ">
-                    <p>{obj?.tutor}</p>
-                    <p className="text-[#5a05c2] text-sm font-bold ">
-                      {obj?.price}
-                    </p>
-                  </div>
+              <img src={obj.img} className="object-cover " />
+              <div className="flex flex-col justify-between h-full gap-10 px-4 py-5">
+                <div>
+                  <span className=" text-[#5A00C7] text-lg font-bold ">
+                    {obj?.title}
+                  </span>
+                  <p className="text-sm line-clamp-2">{obj?.desc}</p>
                 </div>
-              </Link>
-            </div>
+                <div className="flex items-center justify-between ">
+                  <p>{obj?.tutor}</p>
+                  <p className="text-[#5a05c2] text-sm font-bold ">
+                    {obj?.price}
+                  </p>
+                </div>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
