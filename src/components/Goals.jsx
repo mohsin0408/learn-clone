962c20e87219e36a01fa64215c98ad903f329522
mohsinali shaikh <mohsinalishaikh987@gmail.com>
2025-01-30 13:08:00 +0530

    30 jan update

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
 const Goals = () => {
-  return <div>Goals</div>;
+  const goalsData = {
+    heading: "YOUR LEARNING EXPERIENCE",
+    desc: "You got the goal, we got the tools",
+    goalImg: [
+      {
+        img: "https://res.cloudinary.com/academind-gmbh/image/upload/f_auto,q_80/v1606912139/academind.com/site/courses-icon_u9emvz",
+        info: "50 Courses Included",
+      },
+      {
+        img: "https://res.cloudinary.com/academind-gmbh/image/upload/f_auto,q_80/v1606912139/academind.com/site/hours-content-icon_dyye62",
+        info: "> 700+ Hours of HD Video Content",
+      },
+      {
+        img: "https://res.cloudinary.com/academind-gmbh/image/upload/f_auto,q_80/v1606912139/academind.com/site/qa-icon_fzkput",
+        info: "Fast & Friendly Q&A Support",
+      },
+      {
+        img: "https://res.cloudinary.com/academind-gmbh/image/upload/f_auto,q_80/v1606912139/academind.com/site/worldwide-students-icon_bni0jb",
+        info: "> 3,000,000 Students Worldwide",
+      },
+      {
+        img: "https://res.cloudinary.com/academind-gmbh/image/upload/f_auto,q_80/v1606912139/academind.com/site/updated-icon_zfausy",
+        info: "Up-to-Date Courses",
+      },
+      {
+        img: "https://res.cloudinary.com/academind-gmbh/image/upload/f_auto,q_80/v1606912139/academind.com/site/new-courses-icon_a6gwgy",
+        info: "New Courses Added Regularly",
+      },
+    ],
+  };
+  return (
+    <div>
+      <h2 className="text-3xl font-semibold text-center lg:text-4xl ">
+        {goalsData.heading}
+      </h2>
+      <p className="text-lg text-center font-extralight ">{goalsData.desc}</p>
+      <div className="grid items-center justify-center grid-cols-1 gap-5 p-5 lg:grid-cols-3">
+        {goalsData.goalImg.map((img, index) => (
+          <span
+            key={index}
+            className="flex flex-col items-center justify-center gap-5">
+            <img src={img.img} alt="goal" className="w-20 h-20" />
+            <p>{img.info}</p>
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Goals;
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -7,17 +7,20 @@ const Video = () => {
     videoUrl: "https://player.hotmart.com/embed/OLD8mzG1Lx",
   };
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center p-10 ">
+    <>
+      <div className="flex flex-col items-center justify-center p-5 lg:p-10">
         <h2 className="mt-5 mb-2 text-2xl font-semibold text-center lg:text-3xl ">
           {videoData.heading}
         </h2>
         <p className="mb-5 text-lg text-center font-extralight ">
           {videoData.desc}
         </p>
-        <iframe width="560" height="315" src={videoData.videoUrl}></iframe>
+        <iframe
+          height="500"
+          src={videoData.videoUrl}
+          className=" w-[350px] md:w-[560px] lg:w-[950px] "></iframe>
       </div>
-    </div>
+    </>
   );
 };
 
