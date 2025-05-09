66e762919c8b0633d1ad6e34b265a1db1fe91709
mohsinali shaikh <mohsinalishaikh987@gmail.com>
2025-05-10 01:12:15 +0530

    update 10

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,6 +7,8 @@ import { FaRegCircle } from "react-icons/fa6";
 import { useDispatch, useSelector } from "react-redux";
 import { setVideoSrcs, setName } from "./Store/Action";
 import Curriculum from "./Curriculum";
+import { RxHamburgerMenu } from "react-icons/rx";
+import Button from "./Button";
 
 const Dashboard = ({ handlePathname }) => {
   const [lectures, setLectures] = useState(null);
@@ -56,49 +58,47 @@ const Dashboard = ({ handlePathname }) => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, [params.slug, params.id]);
 
-  const handleVideoSrcs = (videoSrc, title, id) => {
-    dispatch(setVideoSrcs(videoSrc));
-    dispatch(setName(title));
-    navigate(`/lectures/${params.slug}/Lectures/${id}`);
-  };
-
   if (loading) return <div className="mt-20 text-center">Loading...</div>;
   if (!lectures)
     return <div className="mt-20 text-center">Lectures not found</div>;
 
   return (
-    <div>
-      <div className="flex">
-        <div className="flex items-center justify-between h-16 p-3 text-2xl text-white bg-black border-r border-white w-[405px]">
+    <div className="">
+      <div className="fixed top-0 flex items-center w-full ">
+        <div className="h-[70px] border-r-white border-r text-white min-w-[380px] flex max-sm:px-10 px-5 items-center justify-between bg-black max-md:w-full">
           <SlHome
             className="cursor-pointer"
             onClick={() => navigate(`/course/${params.slug}`)}
           />
           <IoSettingsOutline />
         </div>
+        <div className="h-[70px] pr-10 bg-black w-full text-white flex items-center justify-end max-md:hidden">
+          <RxHamburgerMenu />
+          <Button text="complete the course" px="30px" py="12px" />
+        </div>
       </div>
 
-      <div className="flex">
-        <div className="w-[380px] h-[550px] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200">
+      <div className="w-full h-[calc(100vh_+_200px)] flex pt-[70px] ">
+        <div className=" w-[380px] h-full overflow-y-auto max-md:hidden scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200">
           <Curriculum dashboard lectures={lectures} />
         </div>
-
-        {/* <div className="flex flex-col items-center justify-center">
-          <div className="flex items-center gap-2 mb-2 text-xl font-medium">
-            <LuTvMinimalPlay />
-            <p>{name}</p>
+        <div className="flex-1 py-10 md:pt-[70px] pt-[30px] flex flex-col items-center gap-4">
+          <div className="p-4">
+            <div className="flex items-center gap-2 mb-2 text-xl font-medium">
+              <LuTvMinimalPlay />
+              <p>{name}</p>
+            </div>
+            <video
+              src={videoSrcs}
+              className="w-[100%]  rounded-lg"
+              autoPlay
+              muted
+              playsInline
+              controls>
+              Your browser does not support the video tag.
+            </video>
           </div>
-          <video
-            src={videoSrcs}
-            width="600"
-            height="400"
-            autoPlay
-            muted
-            playsInline
-            controls>
-            Your browser does not support the video tag.
-          </video>
-        </div> */}
+        </div>
       </div>
     </div>
   );
