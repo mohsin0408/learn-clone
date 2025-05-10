68f85180b78cf269437734cbb9eb0ca1ee04adf1
mohsinali shaikh <mohsinalishaikh987@gmail.com>
2025-05-10 16:29:52 +0530

    update-10

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -11,6 +11,7 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import Button from "./Button";
 
 const Dashboard = ({ handlePathname }) => {
+  const [show, setShow] = useState(false);
   const [lectures, setLectures] = useState(null);
   const [loading, setLoading] = useState(true);
   const params = useParams();
@@ -64,26 +65,33 @@ const Dashboard = ({ handlePathname }) => {
 
   return (
     <div className="">
-      <div className="fixed top-0 flex items-center w-full ">
-        <div className="h-[70px] border-r-white border-r text-white min-w-[380px] flex max-sm:px-10 px-5 items-center justify-between bg-black max-md:w-full">
+      <div className="fixed top-0 flex flex-col items-center w-full md:flex-row ">
+        <div
+          className="h-[70px] md:border-r-white border-r-white md:border-b-black
+         border-b-white border-b-2 border-r  min-w-[380px] flex max-sm:px-10 px-5 items-center justify-between bg-black text-white  max-md:w-full">
           <SlHome
-            className="cursor-pointer"
+            className="cursor-pointer text-[30px]"
             onClick={() => navigate(`/course/${params.slug}`)}
           />
-          <IoSettingsOutline />
+          <IoSettingsOutline className="text-[30px]" />
         </div>
-        <div className="h-[70px] pr-10 bg-black w-full text-white flex items-center justify-end max-md:hidden">
-          <RxHamburgerMenu />
-          <Button text="complete the course" px="30px" py="12px" />
+        <div className="h-[70px] max-sm:px-10 px-5 border-black  bg-black text-white w-full flex items-center justify-between ">
+          <RxHamburgerMenu
+            className="text-[30px] visible md:invisible "
+            onClick={() => {
+              setShow(!show), console.log("click");
+            }}
+          />
+          <Button text="complete" px="30px" py="12px" />
         </div>
       </div>
 
       <div className="w-full h-[calc(100vh_+_200px)] flex pt-[70px] ">
-        <div className=" w-[380px] h-full overflow-y-auto max-md:hidden scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200">
+        <div className=" w-[380px] h-full overflow-y-auto hidden md:block scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200">
           <Curriculum dashboard lectures={lectures} />
         </div>
-        <div className="flex-1 py-10 md:pt-[70px] pt-[30px] flex flex-col items-center gap-4">
-          <div className="p-4">
+        <div className="flex-1 py-10 md:pt-[70px] pt-[80px] flex flex-col items-center gap-4">
+          <div className="px-6 py-7 ">
             <div className="flex items-center gap-2 mb-2 text-xl font-medium">
               <LuTvMinimalPlay />
               <p>{name}</p>
@@ -100,6 +108,11 @@ const Dashboard = ({ handlePathname }) => {
           </div>
         </div>
       </div>
+      {show && (
+        <div className=" fixed top-40 left-0 z-50 w-full h-[calc(100vh-70px)] overflow-y-auto bg-white md:hidden scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200  ">
+          <Curriculum dashboard lectures={lectures} />
+        </div>
+      )}
     </div>
   );
 };
